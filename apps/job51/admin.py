b0815cbf943a65a60fbf669dbc3d8194a8986750
acer-haitao <acer_yuhaitao@163.com>
2018-04-24 15:56:48 +0800

    增加job51--jobaddress字段

--- apps/job51/admin.py
+++ apps/job51/admin.py
@@ -6,7 +6,7 @@ from apps.job51.models import job51
 @admin.register(job51)
 class job51Admin(admin.ModelAdmin):
     #显示
-    list_display = ('id','job','company','address','wages','date','jobname','joburl','jobtxt')
+    list_display = ('id','job','company','address','wages','date','jobname','joburl','jobaddress')
     #显示条数
     list_per_page = 50
     #显示排序
@@ -14,6 +14,6 @@ class job51Admin(admin.ModelAdmin):
     #显示编辑
     #list_editable = ['jobname']
     #过滤
-    list_filter = ('job','address','wages','date','jobname')
-    search_fields = ('job','company','address','wages','date','jobname')
+    list_filter = ('job','address','wages','date','jobname','jobaddress')
+    search_fields = ('job','company','address','wages','date','jobname','jobaddress')
     #date_hierarchy = 'date'
--- apps/job51/models.py
+++ apps/job51/models.py
@@ -10,6 +10,8 @@ class job51(models.Model):
     jobname = models.CharField(max_length=256)
     joburl = models.CharField(max_length=512)
     jobtxt = models.CharField(max_length=512,blank = True)
+    jobaddress = models.CharField(max_length=256,blank = True)
+
     def __str__(self):
         tp = '<job51:[job:{job},company:{company},address:{address},wages:{wages},date:{date},jobname:{jobname},joburl:{joburl},jobtxt:{jobtxt}]>'
         return tp.format(job = self.job,company = self.company,address =self.address,wages = self.wages,date = self.date,jobname = self.jobname,joburl = self.joburl,jobtxt = self.jobtxt )
